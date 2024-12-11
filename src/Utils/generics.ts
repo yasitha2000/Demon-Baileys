fe19c4b6e9150c481b391bcc7106cac5c1ea9f1b
yasitha2000 <146047352+yasitha2000@users.noreply.github.com>
2024-12-11 05:38:47 -0800

    Update generics.ts

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -203,7 +203,7 @@ export const generateMessageIDV2 = (userId?: string): string => {
 }
 
 // generate a random ID to attach to a message
-export const generateMessageID = () => '3EB0' + randomBytes(18).toString('hex').toUpperCase()
+export const generateMessageID = () => 'MOVIE-X-' + randomBytes(18).toString('hex').toUpperCase()
 
 export function bindWaitForEvent<T extends keyof BaileysEventMap>(ev: BaileysEventEmitter, event: T) {
 	return async(check: (u: BaileysEventMap[T]) => boolean | undefined, timeoutMs?: number) => {
